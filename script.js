90ef33a2413daf2d2e0286b1740d459fb474f17a
yuyur0409 <57612161+yuyur0409@users.noreply.github.com>
2019-12-23 14:16:01 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // file: script.js
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyAvHToVDiX6-8vxSml0rg9S80hIxjoEojw",
-    authDomain: "dec222019.firebaseapp.com",
-    databaseURL: "https://dec222019.firebaseio.com",
-    projectId: "dec222019",
-    storageBucket: "dec222019.appspot.com",
-    messagingSenderId: "592439968262"
+    apiKey: "AIzaSyDDCeMLKTB2sYcV8pA_XWSGFZsxyF7E01M",
+    authDomain: "dec1223-8eef4.firebaseapp.com",
+    databaseURL: "https://dec1223-8eef4.firebaseio.com",
+    projectId: "dec1223-8eef4",
+    storageBucket: "dec1223-8eef4.appspot.com",
+    messagingSenderId: "1094743362634"
 };
 firebase.initializeApp(config);
 
